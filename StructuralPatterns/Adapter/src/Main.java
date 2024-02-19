d9fcf158d0488d670c56c52b3f94009d8f7db47e
firdevssenturk <firdevsssenturk@gmail.com>
2024-02-19 17:00:21 +0300

    Adapter Design Pattern is a structural pattern that allows incompatible interfaces to work together

--- StructuralPatterns/Adapter/src/Main.java
+++ StructuralPatterns/Adapter/src/Main.java
@@ -1,5 +1,13 @@
 /*
 The Adapter Design Pattern is a structural pattern that allows incompatible interfaces to work together. It acts as a bridge between two incompatible interfaces by converting the interface of a class into another interface that a client expects
+
+ MediaPlayer is the target interface that clients use to play different types of audio files.
+AdvancedMediaPlayer is the interface that provides specific functionality for playing different types of advanced audio files.
+VlcPlayer and Mp4Player are concrete classes implementing the AdvancedMediaPlayer interface.
+MediaAdapter is the adapter class that implements the MediaPlayer interface by utilizing the AdvancedMediaPlayer interface.
+AudioPlayer is the client class that interacts with the MediaPlayer interface.
+AdapterPatternDemo is the main class where the client code is written to demonstrate the usage of the Adapter Design Pattern.
+
  */
 public class Main {
     public static void main(String[] args) {
